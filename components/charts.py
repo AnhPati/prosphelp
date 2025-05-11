acbba5245f8e3721c4e620db620544535bba15be
Anhpati <romain-felicianne@hotmail.fr>
2025-05-11 20:49:15 +0200

    ✨ COMPONENTS > Add plot chart to charts file.

--- components/charts.py
+++ components/charts.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
 
 def show_market_trend_chart(df):
     if df.empty:
@@ -19,3 +21,24 @@ def show_market_trend_chart(df):
     # Pivot et affichage
     pivot = df_market.pivot(index="Date", columns="Marché", values="Nombre d'annonces").fillna(0)
     st.line_chart(pivot)
+
+def plot_skills_tech_chart(data, title=""):
+    # Compter les occurrences des éléments
+    counter = Counter(data)
+    
+    # Trier par fréquence
+    sorted_items = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+    skills, counts = zip(*sorted_items)
+
+    # Créer un graphique en bâtons
+    plt.figure(figsize=(10, 6))
+    plt.bar(skills, counts, color="skyblue")
+    plt.xticks(rotation=45, ha='right')
+    plt.title(title)
+    plt.xlabel("Compétences / Technologies")
+    plt.ylabel("Fréquence")
+    plt.tight_layout()
+
+    # Afficher le graphique
+    st.pyplot(plt)
+
