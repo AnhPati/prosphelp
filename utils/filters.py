bcf6822818405ad0fe23c8162229f0ea641b549d
Anhpati <romain-felicianne@hotmail.fr>
2025-06-17 20:22:34 +0200

    ♻️ UTILS > Replace text filters by constants values.

--- utils/filters.py
+++ utils/filters.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+from constants.labels import ALL_MARKETS_OPTION
 
 def filter_dataframe_by_market(df: pd.DataFrame, markets: list[str], label: str = "Filtrer par marché") -> pd.DataFrame:
     if markets is None or len(markets) == 0:
         st.warning("⚠️ Aucun marché disponible pour le filtrage.")
-        return "Tous"
+        return ALL_MARKETS_OPTION
 
     selected_market = st.selectbox(f"{label}", markets, index=0)
 
