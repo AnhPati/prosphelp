966f5c80cecfbad63d85eceb6eaffba8f95ff73c
Anhpati <romain-felicianne@hotmail.fr>
2025-06-17 18:42:41 +0200

    💡 APP > Remove comments and add new app name.

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ from tabs.compass import show_compass
 from components.file_controls import files_controls
 from config.settings import MARKET_OFFERS_FILE
 
-st.set_page_config(page_title="Prospection Tracker", layout="wide")
-st.title("Prospection Tracker 🚀")
+st.set_page_config(page_title="JobCompass", layout="wide")
+st.title("JobCompass")
 
-# ✅ Import/export unique dans la sidebar
 files_controls(MARKET_OFFERS_FILE, "Données Offres & Marché", uploader_key="global_data_controls")
 
-# 📑 Onglets de navigation avec accueil
 tab0, tab1, tab2, tab3 = st.tabs([
     "🏠 Accueil",
     "📈 Analyse des marchés",
@@ -20,17 +18,11 @@ tab0, tab1, tab2, tab3 = st.tabs([
     "🧭 Boussole"
 ])
 
-# 🏠 Accueil
 with tab0:
     show_home()
-# 📈 Analyse des marchés
 with tab1:
     show_market_analysis()
-
-# 📝 Dissection des offres
 with tab2:
     show_offer_dissection()
-
-# 🧭 Boussole
 with tab3:
     show_compass()
