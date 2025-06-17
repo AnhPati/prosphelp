eaf9fa6536854cb3b20ef268d663a25963a59b3d
Anhpati <romain-felicianne@hotmail.fr>
2025-06-17 20:48:33 +0200

    💬 CONSTANTS > Add components alerts and labels.

--- constants/alerts.py
+++ constants/alerts.py
@@ -10,14 +10,27 @@ NO_SECONDARY_TECH = "⚠️ Aucune technologie secondaire disponible pour ce mar
 MISSING_MARKET = "⚠️ Merci de spécifier un marché."
 MARKET_ALREADY_EXISTS = "⚠️ Une entrée pour ce marché à cette date existe déjà."
 NO_MARKET_IN_FILE = "⚠️ Aucun marché détecté dans le fichier `offers.csv`. Veuillez d'abord en créer via l'onglet Analyse des marchés."
+TITLE_LINK_REQUIRED = "⚠️ Le titre et le lien de l'offre sont obligatoires."
+CONTACT_NAME_REQUIRED = "⚠️ Le nom du contact est requis."
 
 # Info
 NO_TREND_DATA = "ℹ️ Aucune donnée de tendance disponible pour ce marché."
 NO_RYTHM_DATA = "ℹ️ Aucune donnée sur le rythme de travail pour ce marché."
 NO_SECTOR_DATA = "ℹ️ Aucune donnée sur le secteur de travail pour ce marché."
-NO_ANALYSIS_DATA = "Aucune donnée d'analyse de marché disponible."
-NO_OFFERS_DATA = "Aucune offre enregistrée pour le moment."
+NO_ANALYSIS_DATA = "ℹ️ Aucune donnée d'analyse de marché disponible."
+NO_OFFERS_DATA = "ℹ️ Aucune offre enregistrée pour le moment."
+NO_DATA_AVAILABLE = "ℹ️ Aucune donnée à afficher."
+NO_MARKET_DATA_AVAILABLE = "ℹ️ Aucune donnée de type 'Marché' à afficher."
+NO_COORDINATES_DATA = "ℹ️ Aucune donnée de localisation valide avec des coordonnées géographiques disponible pour ce marché sélectionné."
+MAP_SUMMARY = "ℹ️ Affichage de {n} localisation{s} unique{s} sur la carte pour le marché '{market_name}'. La couleur et la taille des points indiquent le nombre d'offres."
+NO_NUMERIC_DATA = "ℹ️ Aucune donnée numérique valide disponible pour '{column_name}'."
+NO_UNIQUE_VALUES = "ℹ️ Aucune valeur unique disponible pour l'exploration de '{column_name}'."
 
 # Success
 DATAS_SAVED_SUCCESS = "✅ Données ajoutée avec succès."
 OFFER_SAVED_SUCCESS = "✅ Offre enregistrée avec succès !"
+SUCCESS_FILE_IMPORTED = "✅ Fichier importé avec succès."
+
+# Errors
+ERROR_MISSING_TYPE_COLUMN = "❌ Colonne 'Type' introuvable. Le fichier a-t-il bien le bon séparateur ?"
+ERROR_FILE_PROCESSING = "❌ Erreur lors du traitement du fichier : {error}"
