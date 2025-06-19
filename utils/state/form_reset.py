4c32be949706aae917519e503ac979c39746ff18
Anhpati <romain-felicianne@hotmail.fr>
2025-06-19 03:25:40 +0200

    🩹 UTILS > Rename key variables into form reset.

--- utils/state/form_reset.py
+++ utils/state/form_reset.py
@@ -3,9 +3,9 @@ import streamlit as st
 def reset_form_state(prefix: str) -> bool:
     clear_key = f"clear_form_{prefix}"
     if st.session_state.get(clear_key):
-        keys_to_reset = [k for k in st.session_state.keys() if k.startswith(prefix)]
-        for k in keys_to_reset:
-            st.session_state[k] = None if "date" in k else ""
+        keys_to_reset = [key for key in st.session_state.keys() if key.startswith(prefix)]
+        for key in keys_to_reset:
+            st.session_state[key] = None if "date" in key else ""
         st.session_state[clear_key] = False
         return True
     return False
