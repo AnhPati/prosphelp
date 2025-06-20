d19fb91d88b097ea95b83298f1af616d1db6e043
Anhpati <romain-felicianne@hotmail.fr>
2025-06-20 18:10:58 +0200

    💄 CHARTS > Set background color for bar chart using theme variable and add bar chart styles to theme.css.

--- components/charts/bar_chart.py
+++ components/charts/bar_chart.py
@@ -4,6 +4,7 @@ from collections import Counter
 import plotly.express as px
 from constants.labels import X_AXIS_SKILLS_TECH, Y_AXIS_FREQUENCY
 from constants.alerts import INFO_NO_DATA_TO_DISPLAY
+from design.theme_colors import THEME
 
 def bar_chart(data, title="", context_id="default"):
     if data is None or not isinstance(data, pd.Series) or data.empty:
@@ -24,6 +25,11 @@ def bar_chart(data, title="", context_id="default"):
         labels={'x': X_AXIS_SKILLS_TECH, 'y': Y_AXIS_FREQUENCY},
         title=title
     )
-    fig.update_layout(xaxis_tickangle=-45)
+
+    fig.update_layout(
+        xaxis_tickangle=-45,
+        plot_bgcolor=THEME['surface'],      # 🔹 Fond de la zone de tracé
+        paper_bgcolor=THEME['surface']      # 🔹 Fond du canevas global
+    )
 
     st.plotly_chart(fig, use_container_width=True, key=f"bar_chart_{context_id}_{title or 'no_title'}")
