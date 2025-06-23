3e072da117e1873a4c145014b13601847885953d
Anhpati <romain-felicianne@hotmail.fr>
2025-06-23 18:12:22 +0200

    🔧 CONFIG > Add firebase config.

---
+++ config/firebase_config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+firebase_config = {
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": "579820563933",
+    "appId": os.getenv("FIREBASE_APP_ID"),
+}
